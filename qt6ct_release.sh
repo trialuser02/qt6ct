e49a8ca073a9c7ad244f9357971b32dfc30d211b
Ilya Kotov <iokotov@astralinux.ru>
2020-06-30 21:42:29 +0300

    added .gitignore and other files

---
+++ qt6ct_release.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+NAME=qt6ct
+
+MAJOR=`cat src/qt6ct/qt6ct.h | grep "#define QT6CT_VERSION_MAJOR" | cut -d " " -f3`
+MINOR=`cat src/qt6ct/qt6ct.h | grep "#define QT6CT_VERSION_MINOR" | cut -d " " -f3`
+
+VERSION=$MAJOR.$MINOR
+
+TARBALL=$NAME-$VERSION
+
+echo $TARBALL
+
+mkdir -p extras/package/sources
+git archive --format=tar --prefix=$TARBALL/ $VERSION | xz > extras/package/sources/$TARBALL.tar.xz
