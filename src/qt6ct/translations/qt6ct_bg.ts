79e95e7a176af23e866052011ac917eeeeb9a41e
Ilya Kotov <iokotov@astralinux.ru>
2021-09-22 22:50:23 +0300

    updated translations

--- src/qt6ct/translations/qt6ct_bg.ts
+++ src/qt6ct/translations/qt6ct_bg.ts
@@ -801,7 +801,7 @@
     <message>
         <location filename="../troubleshootingpage.ui" line="40"/>
         <source>Remove</source>
-        <translation type="unfinished">Премахване</translation>
+        <translation>Премахване</translation>
     </message>
     <message>
         <location filename="../troubleshootingpage.ui" line="73"/>
