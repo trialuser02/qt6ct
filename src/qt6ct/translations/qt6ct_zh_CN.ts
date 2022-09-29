483f9bc570b710edccdf959820a1ff73342889bf
Ilya Kotov <iokotov@astralinux.ru>
2022-09-29 22:37:46 +0300

    sync translations

--- src/qt6ct/translations/qt6ct_zh_CN.ts
+++ src/qt6ct/translations/qt6ct_zh_CN.ts
@@ -806,7 +806,7 @@
     <message>
         <location filename="../troubleshootingpage.ui" line="40"/>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>移除</translation>
     </message>
     <message>
         <location filename="../troubleshootingpage.ui" line="73"/>
