6d5dab5f76d67e2b7ac08fb6881b9f09e011e83f
Ilya Kotov <iokotov@astralinux.ru>
2022-10-14 20:55:49 +0000

    fixed crash when loading default font (#20)

--- src/qt6ct-qtplugin/qt6ctplatformtheme.cpp
+++ src/qt6ct-qtplugin/qt6ctplatformtheme.cpp
@@ -285,8 +285,8 @@ void Qt6CTPlatformTheme::readSettings()
     settings.endGroup();
 
     settings.beginGroup("Fonts");
-    m_generalFont = settings.value("general", QVariant::fromValue(QPlatformTheme::font(QPlatformTheme::SystemFont))).value<QFont>();
-    m_fixedFont = settings.value("fixed", QVariant::fromValue(QPlatformTheme::font(QPlatformTheme::FixedFont))).value<QFont>();
+    m_generalFont = settings.value("general", QVariant::fromValue(*QGenericUnixTheme::font(QPlatformTheme::SystemFont))).value<QFont>();
+    m_fixedFont = settings.value("fixed", QVariant::fromValue(*QGenericUnixTheme::font(QPlatformTheme::FixedFont))).value<QFont>();
     settings.endGroup();
 
     settings.beginGroup("Interface");
