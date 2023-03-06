9361ec26509260fd8b10ba5004ab8ab0f7f71dde
Ilya Kotov <iokotov@astralinux.ru>
2023-03-07 00:03:36 +0300

    updated copyright

--- src/qt6ct/mainwindow.cpp
+++ src/qt6ct/mainwindow.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2022, Ilya Kotov <forkotov02@ya.ru>
+ * Copyright (c) 2020-2023, Ilya Kotov <forkotov02@ya.ru>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
