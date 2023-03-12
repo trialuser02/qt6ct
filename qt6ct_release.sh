adbf5f146a77e3fcf46e4652b40550c20c86fbfc
Ilya Kotov <iokotov@astralinux.ru>
2023-03-12 15:28:42 +0300

    fixed qt6ct_release.sh

--- qt6ct_release.sh
+++ qt6ct_release.sh
@@ -2,8 +2,8 @@
 
 NAME=qt6ct
 
-MAJOR=`cat src/qt6ct/qt6ct.h | grep "#define QT6CT_VERSION_MAJOR" | cut -d " " -f3`
-MINOR=`cat src/qt6ct/qt6ct.h | grep "#define QT6CT_VERSION_MINOR" | cut -d " " -f3`
+MAJOR=`cat src/qt6ct-common/qt6ct.h | grep "#define QT6CT_VERSION_MAJOR" | cut -d " " -f3`
+MINOR=`cat src/qt6ct-common/qt6ct.h | grep "#define QT6CT_VERSION_MINOR" | cut -d " " -f3`
 
 VERSION=$MAJOR.$MINOR
 
