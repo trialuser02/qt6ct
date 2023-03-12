480ce3d4b873b36e0a506541ef4f26c9292614b9
Ilya Kotov <iokotov@astralinux.ru>
2023-03-12 15:29:29 +0300

    version bump

--- src/qt6ct-common/qt6ct.h
+++ src/qt6ct-common/qt6ct.h
@@ -30,7 +30,7 @@
 #define QT6CT_H
 
 #define QT6CT_VERSION_MAJOR 0
-#define QT6CT_VERSION_MINOR 8
+#define QT6CT_VERSION_MINOR 9
 
 #define QT6CT_TOSTRING(s) #s
 #define QT6CT_STRINGIFY(s)         QT6CT_TOSTRING(s)
